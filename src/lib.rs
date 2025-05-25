53c999c889838db766286bfc1b957d4ac631b877
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-25 22:18:54 +0545

    Lib init"

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::fs::Metadata;
+use std::os::unix::fs::MetadataExt;
+
+/// Converts a file's mode into a `ls -l` style permission string.
+pub fn to_permission_string(metadata: &Metadata) -> String {
+    let mode = metadata.mode();
+    let file_type = match mode & libc::S_IFMT {
+        libc::S_IFREG => '-',
+        libc::S_IFDIR => 'd',
+        libc::S_IFLNK => 'l',
+        libc::S_IFCHR => 'c',
+        libc::S_IFBLK => 'b',
+        libc::S_IFSOCK => 's',
+        libc::S_IFIFO => 'p',
+        _ => '?',
+    };
+
+    let mut perm_string = String::with_capacity(10);
+    perm_string.push(file_type);
+
+    let permission_bits = [
+        (libc::S_IRUSR, 'r'), (libc::S_IWUSR, 'w'), (libc::S_IXUSR, 'x'),
+        (libc::S_IRGRP, 'r'), (libc::S_IWGRP, 'w'), (libc::S_IXGRP, 'x'),
+        (libc::S_IROTH, 'r'), (libc::S_IWOTH, 'w'), (libc::S_IXOTH, 'x'),
+    ];
+
+    for (bit, chr) in permission_bits.iter() {
+        perm_string.push(if mode & bit != 0 { *chr } else { '-' });
+    }
+
+    perm_string
+}
+
